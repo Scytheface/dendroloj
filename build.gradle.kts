4213c06541f475edab786d988c2a10ce76828d89
Juhan Oskar Hennoste <juhanoskar@gmail.com>
2023-09-08 19:29:59 +0300

    Add automatic versioning based on number of git commits on master branch

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,29 @@
+import java.io.ByteArrayOutputStream
+
 plugins {
     id("com.github.johnrengelman.shadow") version "8.1.1"
     id("java")
     id("org.openjfx.javafxplugin") version "0.0.13"
 }
 
-group = "ee.ut"
-version = "0.1.0"
+fun runCommand(vararg args: String): String {
+    val os = ByteArrayOutputStream()
+    project.exec {
+        commandLine = args.asList()
+        standardOutput = os
+    }
+    return os.toString().trim()
+}
+
+group = "ee.ut.dendroloj"
+val headDescription = runCommand("git", "describe", "--all", "--always", "--candidates=1",  "--match", "master", "--dirty")
+version = if (headDescription == "heads/master") {
+    // If we are on master branch and there are no uncommitted changes, set version to the number of commits counting from the first commit on this branch.
+    "v" + runCommand("git", "rev-list", "HEAD" , "--first-parent", "--count")
+} else {
+    // Otherwise set version to git commit hash with -dirty suffix if there are uncommitted changes.
+    runCommand("git", "rev-parse", "--short", "HEAD") + if (headDescription.endsWith("-dirty")) "-dirty" else "";
+}
 
 repositories {
     mavenCentral()
