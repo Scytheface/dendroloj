eb94cb6db608ef7cab60de8592503431681c9499
Juhan Oskar Hennoste <juhanoskar@gmail.com>
2023-10-30 19:38:39 +0200

    Update array-based binary heap example

--- src/test/java/KuhjaKatsed.java
+++ src/test/java/KuhjaKatsed.java
@@ -4,6 +4,16 @@ import java.util.List;
 
 public class KuhjaKatsed {
 
+    private static int vasakAlluv(List<Integer> kuhi, int i) {
+        int v = 2 * i + 1;
+        return v < kuhi.size() ? v : -1;
+    }
+
+    private static int paremAlluv(List<Integer> kuhi, int i) {
+        int p = 2 * i + 2;
+        return p < kuhi.size() ? p : -1;
+    }
+
     public static void main(String[] args) {
         Dendrologist.setUIScale(1.5);
 
@@ -11,8 +21,8 @@ public class KuhjaKatsed {
         Dendrologist.drawBinaryTree(
                 0,
                 i -> String.valueOf(kuhi.get(i)),
-                i -> 2 * i + 1 < kuhi.size() ? 2 * i + 1 : null,
-                i -> 2 * i + 2 < kuhi.size() ? 2 * i + 2 : null
+                i -> vasakAlluv(kuhi, i) == -1 ? null : vasakAlluv(kuhi, i),
+                i -> paremAlluv(kuhi, i) == -1 ? null : paremAlluv(kuhi, i)
         );
 
     }
