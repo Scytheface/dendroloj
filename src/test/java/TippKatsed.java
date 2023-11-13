f876571e80490939483bc80a38cfd3b2e693d209
Juhan Oskar Hennoste <juhanoskar@gmail.com>
2023-11-13 22:05:19 +0200

    Change tree example to use random generation

--- src/test/java/TippKatsed.java
+++ src/test/java/TippKatsed.java
@@ -1,21 +1,29 @@
 import ee.ut.dendroloj.Dendrologist;
 
+import java.util.*;
+
 public class TippKatsed {
+
+    static Tipp juhuslikPuu(int n, Random juhus) {
+        if (n == 0) return null;
+        int vasakule = juhus.nextInt(n);
+        return new Tipp("", juhuslikPuu(vasakule, juhus), juhuslikPuu(n - 1 - vasakule, juhus));
+    }
+
+    static int nummerdaKeskjärjestuses(Tipp tipp, int i) {
+        if (tipp == null) return i;
+        i = nummerdaKeskjärjestuses(tipp.v, i);
+        tipp.info = Integer.toString(i);
+        i += 1;
+        return nummerdaKeskjärjestuses(tipp.p, i);
+    }
 
     public static void main(String[] args) {
         Dendrologist.setUIScale(1.5);
 
-        Tipp tipp = new Tipp("juur",
-                new Tipp("A",
-                        new Tipp("B", null, new Tipp("C")),
-                        new Tipp("DendroloJ",
-                                new Tipp("D"),
-                                new Tipp("E", new Tipp("F"), new Tipp("G")))),
-                null
-        );
-        //tipp.v.v.p.p = tipp.v.p.v;
-        //tipp.v.v.v = tipp.v.v.p;
-        //tipp.v.p.v.p = tipp.v;
+        Tipp tipp = juhuslikPuu(20, new Random(42));
+        nummerdaKeskjärjestuses(tipp, 1);
+
         Dendrologist.drawBinaryTree(tipp, t -> t.info, t -> t.v, t -> t.p);
         // Dendrologist.drawBinaryTree(tipp, t -> t.info + " x=" + t.x, t -> t.v, t -> t.p);
     }
