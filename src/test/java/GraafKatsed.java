1cb10324d7e878a27bbcf274c81a94d26489b903
Juhan Oskar Hennoste <juhanoskar@gmail.com>
2023-12-09 18:57:40 +0200

    Tweak graph drawing examples

--- src/test/java/GraafKatsed.java
+++ src/test/java/GraafKatsed.java
@@ -8,48 +8,47 @@ import java.util.List;
 public class GraafKatsed {
 
     public static void main(String[] args) {
-        // Note: The API for drawing directly from an adjacency matrix is experimental and may eventually be removed.
-        String[] nimed = new String[]{
-                "A",
-                "B",
-                "C",
-                "D",
-                "E"
-        };
-        int[][] M = {
-                {0, 11, 1, 12, -1},
-                {4, 0, 2, -1, 6},
-                {-1, 2, 0, 3, -1},
-                {12, -1, -1, 0, -1},
-                {-1, -1, 5, 4, 123}
-        };
         {
+            String[] nimed = new String[]{
+                    "A",
+                    "B",
+                    "C",
+                    "D",
+                    "E"
+            };
+            int[][] M = {
+                    {0, 11, 1, 12, -1},
+                    {4, 0, 2, -1, 6},
+                    {-1, 2, 0, 3, -1},
+                    {12, -1, -1, 0, -1},
+                    {-1, -1, 5, 4, 123}
+            };
+
             GraphCanvas<Integer> lõuend = new GraphCanvas<>();
             for (int i = 0; i < M.length; i++) {
                 lõuend.drawVertex(i, nimed[i]);
             }
             for (int i = 0; i < M.length; i++) {
                 for (int j = 0; j < M.length; j++) {
-                    if (i != j && M[i][j] != - 1) lõuend.drawDirectedEdge(i, j, String.valueOf(M[i][j]));
+                    if (i != j && M[i][j] != -1) lõuend.drawDirectedEdge(i, j, String.valueOf(M[i][j]));
                 }
             }
             Dendrologist.drawGraph(lõuend);
         }
 
-        List<Tipp> tipud = new ArrayList<>();
-        tipud.add(new Tipp("A", 1, 0));
-        tipud.add(new Tipp("B", 2, 5));
-        tipud.add(new Tipp("C", 0, 1));
-        tipud.add(new Tipp("D", 1, 1));
-        tipud.add(new Tipp("E", 0, 0));
-
-        tipud.get(0).kaared.add(new Kaar(11, tipud.get(1)));
-        tipud.get(0).kaared.add(new Kaar(22, tipud.get(2)));
-        tipud.get(3).kaared.add(new Kaar(31, tipud.get(2)));
-        tipud.get(4).kaared.add(new Kaar(0, tipud.get(4)));
-        tipud.get(4).kaared.add(new Kaar(7, tipud.get(2)));
-
         {
+            List<Tipp> tipud = new ArrayList<>();
+            tipud.add(new Tipp("A", 1, 0));
+            tipud.add(new Tipp("B", 2, 5));
+            tipud.add(new Tipp("C", 0, 1));
+            tipud.add(new Tipp("D", 1, 1));
+            tipud.add(new Tipp("E", 0, 0));
+            tipud.get(0).kaared.add(new Kaar(11, tipud.get(1)));
+            tipud.get(0).kaared.add(new Kaar(22, tipud.get(2)));
+            tipud.get(3).kaared.add(new Kaar(31, tipud.get(2)));
+            tipud.get(4).kaared.add(new Kaar(0, tipud.get(4)));
+            tipud.get(4).kaared.add(new Kaar(7, tipud.get(2)));
+
             GraphCanvas<Tipp> lõuend = new GraphCanvas<>();
             for (Tipp tipp : tipud) {
                 lõuend.drawFixedVertex(tipp, tipp.tähis, tipp.x, tipp.y);
