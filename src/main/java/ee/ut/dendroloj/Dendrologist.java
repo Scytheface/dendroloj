d696642bde15804d5a7e045adf9674f5149b79a9
Juhan Oskar Hennoste <juhanoskar@gmail.com>
2024-03-27 18:52:35 +0200

    Set default UI scale to 2.0

--- src/main/java/ee/ut/dendroloj/Dendrologist.java
+++ src/main/java/ee/ut/dendroloj/Dendrologist.java
@@ -13,7 +13,7 @@ public final class Dendrologist {
     private static boolean awake = false;
 
     // Startup settings
-    private static double uiScale = 1.5;
+    private static double uiScale = 2.0;
 
     // Runtime settings
     static boolean showMethodNames = false;
@@ -26,7 +26,7 @@ public final class Dendrologist {
     /**
      * Sets multiplier for size of UI elements (text, nodes, etc.).
      * <p>
-     * Default: {@code 1.5}
+     * Default: {@code 2.0}
      */
     public static void setUIScale(double uiScale) {
         if (awake) {
